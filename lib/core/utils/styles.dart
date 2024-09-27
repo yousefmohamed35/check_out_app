59fdd9b47dc31141a938d00a7de824861bffb1ea
yousefmohamed35 <yousefmo2021@gmail.com>
2024-09-27 16:05:59 +0300

    finish app bar

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class Styles {
   static TextStyle textStyle25 = const TextStyle(
-    fontSize: 25.0,
+    fontSize: 25,
     fontWeight: FontWeight.w500,
     color: Colors.black,
     fontFamily: 'Inter',
@@ -25,7 +25,7 @@ abstract class Styles {
     color: Colors.black,
     fontFamily: 'Inter',
   );
-  static TextStyle textStyle20 =  TextStyle(
+  static TextStyle textStyle20 = TextStyle(
     fontSize: 20.0,
     fontWeight: FontWeight.w400,
     color: Colors.black.withOpacity(0.80000),
--- lib/features/checkout/peresentation/views/my_cart_view.dart
+++ lib/features/checkout/peresentation/views/my_cart_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:paymentapp/core/utils/styles.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
@@ -7,7 +8,12 @@ class MyCartView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Cart'),
+        title: Text(
+          'My Cart',
+          style: Styles.textStyle25,
+        ),
+        centerTitle: true,
+        leading: Icon(Icons.arrow_back),
       ),
     );
   }
