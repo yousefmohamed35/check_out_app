24d4e8f2649787f67ca2a4d8225223e69ca85ea0
yousefmohamed35 <yousefmo2021@gmail.com>
2024-09-27 15:23:48 +0300

    create checkout app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CheckOutApp());
+}
+class CheckOutApp extends StatelessWidget {
+  const CheckOutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
