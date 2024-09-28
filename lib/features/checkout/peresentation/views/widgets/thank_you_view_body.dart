393d6ee62beb7b49f17068d9699ce96f9e9c5cd3
yousefmohamed35 <yousefmo2021@gmail.com>
2024-09-28 21:49:13 +0300

    create dashed line

--- lib/features/checkout/peresentation/views/widgets/thank_you_view_body.dart
+++ lib/features/checkout/peresentation/views/widgets/thank_you_view_body.dart
@@ -6,8 +6,9 @@ class ThankYouViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(32),
+      padding: const EdgeInsets.all(20),
       child: Stack(
+        clipBehavior: Clip.none,
         children: [
           Container(
             decoration: ShapeDecoration(
@@ -15,6 +16,24 @@ class ThankYouViewBody extends StatelessWidget {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(16),
               ),
+            ),
+          ),
+          Positioned(
+            left: 28,
+            right: 28,
+            bottom: MediaQuery.sizeOf(context).height * .2 + 20,
+            child: Row(
+              children: List.generate(
+                  30,
+                  (index) => Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 2),
+                          child: Container(
+                            color: Color(0xffB8B8B8),
+                            height: 2,
+                          ),
+                        ),
+                      )),
             ),
           ),
           Positioned(
@@ -31,6 +50,24 @@ class ThankYouViewBody extends StatelessWidget {
               backgroundColor: Colors.white,
             ),
           ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: -50,
+            child: const CircleAvatar(
+              radius: 50,
+              backgroundColor: const Color(0xffD9D9D9),
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor: Color(0xff34A853),
+                child: Icon(
+                  Icons.check,
+                  color: Colors.white,
+                  size: 50,
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
