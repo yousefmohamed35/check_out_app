87126afa32ee461d39e4d14c08a4c3290a47b453
yousefmohamed35 <yousefmo2021@gmail.com>
2024-09-28 21:58:34 +0300

    refactore the code

--- lib/features/checkout/peresentation/views/widgets/thank_you_view_body.dart
+++ lib/features/checkout/peresentation/views/widgets/thank_you_view_body.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'custom_check_icon.dart';
+import 'custom_dashed_line.dart';
+import 'thank_you_card.dart';
+
 class ThankYouViewBody extends StatelessWidget {
   const ThankYouViewBody({super.key});
 
@@ -10,31 +14,12 @@ class ThankYouViewBody extends StatelessWidget {
       child: Stack(
         clipBehavior: Clip.none,
         children: [
-          Container(
-            decoration: ShapeDecoration(
-              color: const Color(0xffD9D9D9),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-            ),
-          ),
+         const ThankYouCard(),
           Positioned(
             left: 28,
             right: 28,
             bottom: MediaQuery.sizeOf(context).height * .2 + 20,
-            child: Row(
-              children: List.generate(
-                  30,
-                  (index) => Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 2),
-                          child: Container(
-                            color: Color(0xffB8B8B8),
-                            height: 2,
-                          ),
-                        ),
-                      )),
-            ),
+            child:const CustomDashedLine(),
           ),
           Positioned(
             left: -20,
@@ -54,22 +39,11 @@ class ThankYouViewBody extends StatelessWidget {
             left: 0,
             right: 0,
             top: -50,
-            child: const CircleAvatar(
-              radius: 50,
-              backgroundColor: const Color(0xffD9D9D9),
-              child: CircleAvatar(
-                radius: 40,
-                backgroundColor: Color(0xff34A853),
-                child: Icon(
-                  Icons.check,
-                  color: Colors.white,
-                  size: 50,
-                ),
-              ),
-            ),
+            child: CustomCheckIcon(),
           )
         ],
       ),
     );
   }
 }
+
