7302b109c5834377179496a20a3b48ad8507233b
yousefmohamed35 <yousefmo2021@gmail.com>
2024-09-28 20:24:20 +0300

    refactore payment details method body

--- lib/features/checkout/peresentation/views/widgets/payment_details_view_body.dart
+++ lib/features/checkout/peresentation/views/widgets/payment_details_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:paymentapp/core/utils/widgets/custom_button.dart';
 import 'package:paymentapp/features/checkout/peresentation/views/widgets/custom_credit_card.dart';
 import 'package:paymentapp/features/checkout/peresentation/views/widgets/payment_methods_list_view.dart';
 
@@ -7,17 +9,25 @@ class PaymentDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            PaymentMethodsListView(),
-            CustomCreditCard(),
-          ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: CustomScrollView(slivers: [
+        const SliverToBoxAdapter(
+          child: PaymentMethodsListView(),
         ),
-      ),
+        const SliverToBoxAdapter(
+          child: CustomCreditCard(),
+        ),
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 12),
+                child: CustomButton(onTap: () {}),
+              )),
+        )
+      ]),
     );
   }
 }
-
