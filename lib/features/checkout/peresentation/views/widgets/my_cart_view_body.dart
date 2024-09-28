e19ec3a9054902242d1a8a92958233a2e886cbbe
yousefmohamed35 <yousefmo2021@gmail.com>
2024-09-29 00:06:29 +0300

    finish check out payment ui

--- lib/features/checkout/peresentation/views/widgets/my_cart_view_body.dart
+++ lib/features/checkout/peresentation/views/widgets/my_cart_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:paymentapp/core/utils/widgets/custom_button.dart';
 import 'package:paymentapp/features/checkout/peresentation/views/payment_details_view.dart';
 import 'package:paymentapp/features/checkout/peresentation/views/widgets/order_info_item.dart';
+import 'package:paymentapp/features/checkout/peresentation/views/widgets/payment_methods_list_view.dart';
 import 'package:paymentapp/features/checkout/peresentation/views/widgets/total_price_item.dart';
 
 class MyCartViewBody extends StatelessWidget {
@@ -52,20 +53,37 @@ class MyCartViewBody extends StatelessWidget {
           ),
           CustomButton(
             onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
+              showModalBottomSheet(
+                  context: context,
                   builder: (context) {
-                    return const PaymentDetailsView();
-                  },
-                ),
-              );
+                    return const PaymentMethodsBottomSheet();
+                  });
             },
             text: 'Complete payment',
           ),
           const SizedBox(
             height: 12,
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class PaymentMethodsBottomSheet extends StatelessWidget {
+  const PaymentMethodsBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(height: 16.0),
+          const PaymentMethodsListView(),
+          const SizedBox(height: 32.0),
+          CustomButton(onTap: () {}, text: 'continue'),
         ],
       ),
     );
