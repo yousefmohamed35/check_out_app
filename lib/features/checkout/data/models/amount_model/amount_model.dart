91551ec4608f0881b0dceb48cf638d2b1ebf94d5
yousefmohamed35 <yousefmo2021@gmail.com>
2024-10-09 13:13:35 +0300

    create transaction model

--- lib/features/checkout/data/models/amount_model/amount_model.dart
+++ lib/features/checkout/data/models/amount_model/amount_model.dart
@@ -1,18 +1,17 @@
 import 'details.dart';
 
 class AmountModel {
-  String? total;
-  String? currency;
-  Details? details;
+  final String total;
+  final String currency;
+  final Details details;
 
-  AmountModel({this.total, this.currency, this.details});
+  AmountModel(
+      {required this.total, required this.currency, required this.details});
 
   factory AmountModel.fromJson(Map<String, dynamic> json) => AmountModel(
-        total: json['total'] as String?,
-        currency: json['currency'] as String?,
-        details: json['details'] == null
-            ? null
-            : Details.fromJson(json['details'] as Map<String, dynamic>),
+        total: json['total'] as String,
+        currency: json['currency'] as String,
+        details: Details.fromJson(json['details'] as Map<String, dynamic>),
       );
 
   Map<String, dynamic> toJson() => {
--- lib/features/checkout/peresentation/views/widgets/custom_button_bloc_consumer.dart
+++ lib/features/checkout/peresentation/views/widgets/custom_button_bloc_consumer.dart
@@ -4,9 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:paymentapp/core/utils/widgets/custom_button.dart';
+import 'package:paymentapp/features/checkout/data/models/amount_model/amount_model.dart';
+import 'package:paymentapp/features/checkout/data/models/item_list_model/item.dart';
+import 'package:paymentapp/features/checkout/data/models/item_list_model/item_list_model.dart';
 import 'package:paymentapp/features/checkout/data/models/payment_intent_input_model.dart';
+
 import 'package:paymentapp/features/checkout/peresentation/manger/payment_cubit.dart';
 import 'package:paymentapp/features/checkout/peresentation/views/thank_you_view.dart';
+
+import '../../../data/models/amount_model/details.dart';
 
 class CustomButtonBlocConsumer extends StatelessWidget {
   const CustomButtonBlocConsumer({
@@ -46,43 +52,47 @@ class CustomButtonBlocConsumer extends StatelessWidget {
             // );
             // BlocProvider.of<PaymentCubit>(context)
             //     .makePayment(paymentIntentInputModel: paymentIntentInputModel);
+            List<OrderItemModel> orders = [
+              OrderItemModel(
+                name: "Apple",
+                quantity: 4,
+                price: "10",
+                currency: "USD",
+              ),
+              OrderItemModel(
+                name: "Pineapple",
+                quantity: 5,
+                price: "12",
+                currency: "USD",
+              ),
+            ];
+
+            var itemList = ItemListModel(
+              orders: orders,
+            );
+            var amount = AmountModel(
+              total: "100",
+              currency: "USD",
+              details: Details(
+                subtotal: "100",
+                shipping: "0",
+                shippingDiscount: 0,
+              ),
+            );
             Navigator.of(context).push(MaterialPageRoute(
               builder: (BuildContext context) => PaypalCheckoutView(
                 sandboxMode: true,
                 clientId: "YOUR CLIENT ID",
                 secretKey: "YOUR SECRET KEY",
-                transactions: const [
+                transactions: [
                   {
-                    "amount": {
-                      "total": "100",
-                      "currency": "USD",
-                      "details": {
-                        "subtotal": "100",
-                        "shipping": "0",
-                        "shipping_discount": 0
-                      }
-                    },
+                    "amount": amount.toJson(),
                     "description": "The payment transaction description.",
                     // "payment_options": {
                     //   "allowed_payment_method":
                     //       "INSTANT_FUNDING_SOURCE"
                     // },
-                    "item_list": {
-                      "items": [
-                        {
-                          "name": "Apple",
-                          "quantity": 4,
-                          "price": "10",
-                          "currency": "USD"
-                        },
-                        {
-                          "name": "Pineapple",
-                          "quantity": 5,
-                          "price": "12",
-                          "currency": "USD"
-                        }
-                      ]
-                    }
+                    "item_list": itemList.toJson(),
                   }
                 ],
                 note: "Contact us for any questions on your order.",
